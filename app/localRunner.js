33a33cc6aa5727324399d2195758f52caedbff6b
Daniel Junior <junior.dalcoli@hotmail.com>
2023-07-21 22:18:57 -0300

    :sparkles: Creating request body

--- app/localRunner.js
+++ app/localRunner.js
@@ -1,11 +1,17 @@
-const handler = require('.')
+const handler = require('.');
 
-const event = ''
+const event = {
+    body: JSON.stringify({
+        latitude: '-23.5557714',
+        longitude: '-46.6395571',
+        unitMeasurement: 'metric',
+    })
+}
 
 handler.handler(event)
     .then((res) => {
-        console.log('Local res: ', res)
+        console.log('Local res: ', res);
     })
     .catch((err) => {
-        console.log('Local res: ', err)
+        console.log('Local res: ', err);;
     })
