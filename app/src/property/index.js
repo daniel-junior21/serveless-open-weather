fddd7157cb5c05bf4401c1a30e28fe70eca0824b
Daniel Junior <junior.dalcoli@hotmail.com>
2023-07-21 22:16:34 -0300

    :closed_lock_with_key: adding appId parameter

--- app/src/property/index.js
+++ app/src/property/index.js
@@ -4,3 +4,4 @@ exports.TEAM = 'test'
 exports.NAME_APP = 'openWeather'
 exports.ENV = process.env.NODE_ENV
 exports.VERSION = process.env.npm_package_version
+exports.OPEN_WEATHER_API_KEY = '78ea26a82038b2763850f55fd1f206f0'
